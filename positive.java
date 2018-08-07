df4e567209540e8ff0893e2ba06891316898948f
kuttyanani <42170052+kuttyanani@users.noreply.github.com>
2018-08-07 14:29:05 +0530

    Create positive.java

---
+++ positive.java
@@ -0,0 +1,16 @@
+class positive
+{
+public static void main(String args[])
+{
+int number=109;
+if(number>0)
+{
+system.out.println("positive");
+}
+if(number<0)
+{
+system.out.println("nrgative")
+}
+else
+system.out.println("zero");
+}
